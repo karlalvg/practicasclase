daf5713fcebb5a4aa584dc204a7a8c8445d4f183
LAB-2 <LAB-2@DESKTOP-9OQBEH7>
2022-11-07 17:30:10 -0500

    clases

---
+++ src/main/java/clase_07_011/principal_07_11.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_07_011;
+
+/**
+ *
+ * @author LAB-2
+ */
+public class principal_07_11 {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
